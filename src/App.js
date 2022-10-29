d36e70b767c9e667a6d1f8b1a6caae9302ddd5c2
aonikpaulalock <alockpaul9845@gmail.com>
2022-10-29 18:08:14 +0600

    skills pages completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Routes, Route} from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Exprience from "./Components/DynamicComponents/Exprience";
+import Slidebar from "./Components/DynamicComponents/Slidebar";
+import Skills from "./Components/DynamicComponents/Skills";
+import PersonalDetails from "./Components/DynamicComponents/PersonalDetails";
+import Education from "./Components/DynamicComponents/Education";
+import Certificate from "./Components/DynamicComponents/Certificate";
+import Profile from "./Components/DynamicComponents/Profile";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-left">please make a new folder for yourself</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/slider" element={<Slidebar />}>
+          <Route index element={<Skills/>}></Route>
+          <Route path="details" element={<PersonalDetails/>}></Route>
+          <Route path="education" element={<Education/>}></Route>
+          <Route path="exprience" element={<Exprience/>}></Route>
+          <Route path="certificate" element={<Certificate/>}></Route>
+          <Route path="profile" element={<Profile/>}></Route>
+        </Route>
+      </Routes>
+    </>
   );
 }
 
