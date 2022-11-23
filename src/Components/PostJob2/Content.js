8f68b1ebd76cc89f1822ac96c1d6f167e45467c7
simrellaagrawal <simsim44144@gmail.com>
2022-11-23 16:38:44 +0530

    chnges made

--- src/Components/PostJob2/Content.js
+++ src/Components/PostJob2/Content.js
@@ -63,7 +63,8 @@ const Content = () => {
                   id=""
                   cols="50"
                   rows="4"
-                  className=" pt-1 desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 desktopMd:h-20 mobileMd:h-7 tablet:h-14 laptopMd:h-20 border-solid border-2  border-gray-300 rounded-lg pl-2 bg-gray-100"
+                  className=" pt-1 desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 desktopMd:h-20 mobileMd:h-7
+                   tablet:h-14 laptopMd:h-20 border-solid border-2  border-gray-300 rounded-lg pl-2 bg-gray-100"
                 ></textarea>
               </div>
               <p className="mobileMd:text-sm laptopMd:text-2xl desktopMd:text-xl not-italic font-semibold leading-7 pt-2">
@@ -77,7 +78,8 @@ const Content = () => {
                   id=""
                   cols="50"
                   rows="4"
-                  className=" pt-1 desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 desktopMd:h-20 mobileMd:h-7 tablet:h-14 laptopMd:h-20 border-solid border-2  border-gray-300 rounded-lg pl-2 bg-gray-100"
+                  className=" pt-1 desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 desktopMd:h-20 mobileMd:h-7 
+                  tablet:h-14 laptopMd:h-20 border-solid border-2  border-gray-300 rounded-lg pl-2 bg-gray-100"
                 ></textarea>
               </div>
               <p className="mobileMd:text-sm laptopMd:text-2xl desktopMd:text-xl not-italic font-semibold leading-7 pt-2">
@@ -88,14 +90,17 @@ const Content = () => {
                 onChange={handleSkills}
                 type="text"
                 placeholder="Search skills"
-                className="desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 h-7 border-solid border-2  border-gray-300 rounded-lg pl-2 bg-gray-100"
+                className="desktopMd:w-96 mobileMd:w-60 tablet:w-80 laptopMd:w-480 h-7 border-solid border-2
+                  border-gray-300 rounded-lg pl-2 bg-gray-100"
               />
             </div>
             <div>
-            <button className=" border-solid border-2 border-gray-300 rounded-2xl bg-gray-200 p-1 px-3 mt-4 mr-4 mobileMd:text-xs mobileMd:px-2">
+            <button className=" border-solid border-2 border-gray-300 rounded-2xl bg-gray-200 p-1 px-3 mt-4 
+            mr-4 mobileMd:text-xs mobileMd:px-2">
               Figma
             </button>
-            <button className=" border-solid border-2 border-gray-300 rounded-2xl bg-gray-200 p-1  px-3 mt-4 mobileMd:text-xs mobileMd:px-2">
+            <button className=" border-solid border-2 border-gray-300 rounded-2xl bg-gray-200 p-1  px-3 mt-4 
+            mobileMd:text-xs mobileMd:px-2">
               UI Design
             </button>
             </div>
--- src/Components/empsignup/Signup.js
+++ src/Components/empsignup/Signup.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './Signup.css'
 import {Link} from 'react-router-dom'
-import Navbar from './Navbar'
-
 const SignUp = () => {
   return (
     <>
-    <div className=' bg-gray-100 w-[386px] sm:w-[714px] h-[600px] absolute top-[2%] tablet:top-[2%] laptopMd:top-[60px] left-[0px] tablet:left-[30px] laptopMd:left-[404px]  container1 '>
+    <div className=' box-border bg-gray-100 w-[386px] sm:w-[714px] h-[600px] absolute top-[2%] tablet:top-[2%] laptopMd:top-[60px] left-[0px] tablet:left-[30px] laptopMd:left-[404px]  rounded-2xl container1 '>
       <h2 className='absolute top-[50px] left-[130px] sm:left-[270px]  font-[400] text-[36px]'>Signup!</h2>
       <form>
         <div className=' inline absolute  top-[120px] left-[10%]'>
@@ -25,12 +23,12 @@ const SignUp = () => {
         </div>
 
         <div className='inline absolute top-[320px] left-[10%]'>
-        <label htmlFor="email" className='font-[400] text-[20px] ml-[0px] mb-[10px]'>email id</label><br/>
+        <label htmlFor="email" className='font-[400] text-[20px] ml-[0px] mb-[10px]'>Email Id</label><br/>
         <input type="text" name='firstname' className='w-[315px] sm:w-[570px] h-[50px]  border border-solid rounded-2xl' />
         </div>
 
         <div className='inline absolute top-[420px] left-[10%]'>
-        <label htmlFor="name" className='font-[400] text-[20px] ml-[0px] mb-[10px]'>password</label><br/>
+        <label htmlFor="name" className='font-[400] text-[20px] ml-[0px] mb-[10px]'>Password</label><br/>
         <input type="text" name='firstname' className='w-[315px] sm:w-[570px] h-[50px]  border border-solid rounded-2xl' />
         </div>
        <Link to='/empreg'>
