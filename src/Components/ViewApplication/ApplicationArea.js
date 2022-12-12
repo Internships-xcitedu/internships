b52365ad5a572396c4e83c0eb0ade739baf59d00
AfsanaAbsar <afsanaabsarchy@gmail.com>
2022-12-12 19:55:53 +0600

    data created

--- src/Components/ViewApplication/ApplicationArea.js
+++ src/Components/ViewApplication/ApplicationArea.js
@@ -3,7 +3,32 @@ import homeIcon from '../../Asset/images/HomeIcon.png'
 import calendarIcon from '../../Asset/images/CalendarIcon.png'
 import bookmarkIcon from '../../Asset/images/bookmark.png'
 const ApplicationArea = () => {
-
+    const workDetails = [
+        {
+            "companyName": "Diggligence",
+            "workType": "Work from home",
+            "position": "UI/UX Design",
+            "duration": "2 Months"
+        },
+        {
+            "companyName": "Diggligence",
+            "workType": "Work from home",
+            "position": "UI/UX Design",
+            "duration": "2 Months"
+        },
+        {
+            "companyName": "Diggligence",
+            "workType": "Work from home",
+            "position": "UI/UX Design",
+            "duration": "2 Months"
+        },
+        {
+            "companyName": "Diggligence",
+            "workType": "Work from home",
+            "position": "UI/UX Design",
+            "duration": "2 Months"
+        }
+    ]
     return (
         <>
 
