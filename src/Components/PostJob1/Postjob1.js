4fab139beea816ee154f76d7865ac4b694946b58
SaakshiChopra <sakshichopra202@gmail.com>
2022-12-28 20:20:10 +0530

    postjob1

--- src/Components/PostJob1/Postjob1.js
+++ src/Components/PostJob1/Postjob1.js
@@ -1,19 +1,27 @@
-/*import React from "react";
-import Content from "../PostJob2/Content";
-//import Navbar from "../PostJob2/Navbar";
-//import Sidebar from "../PostJob2/Sidebar";
+import React from "react";
+
+import RNavbar from "../Bars/RNavbar";
+import Navbar from "../Bars/Navbar";
+import Sidebar from "../Bars/Sidebar";
+import Content2 from "./Content2";
 
 const Postjob1 = () => {
   return (
     <>
       <div className="postjob2container">
         <Navbar />
+        <div>
+          <RNavbar />
+        </div>
         <div className="postjob2">
           <Sidebar />
+          <div>
+            <Content2 />
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default Postjob1;*/
+export default Postjob1;
